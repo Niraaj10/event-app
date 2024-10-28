3116b1dc268598082e5c0a598be343ef51d0c99f
Niraaj10 <nitcode10@gmail.com>
2024-10-28 17:00:53 +0530

    AsyncHandler Utils file created

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn();
+    } catch (error) {
+        // console.error(error);
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
