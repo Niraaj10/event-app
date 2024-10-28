b8fdeadd28d38f416664bb5169cd370539e1880e
Niraaj10 <nitcode10@gmail.com>
2024-10-28 18:29:54 +0530

    Database connection file created

---
+++ server/src/database/dbConnect.js
@@ -0,0 +1,17 @@
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        console.log(process.env.MONGODB_URL)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
+
+        console.log(`DB is connected to ${connectionInstance.connection.host}`);
+        // console.log(connectionInstance)
+    } catch (error) {
+        console.log("DB connection error", error);
+        process.exit();
+    }
+}
+
+export default connectDB;
