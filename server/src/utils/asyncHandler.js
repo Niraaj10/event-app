a7ff19be2e8b1475518e65d91890b8260c39ec4b
Niraaj10 <nitcode10@gmail.com>
2024-10-28 17:43:48 +0530

    Updated asyncHandler

--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,12 +1,9 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn();
-    } catch (error) {
-        // console.error(error);
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message,
-        });
+const asyncHandler = (reqHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next))
+        .catch((err) => next(err))
     }
 }
 
+
+export { asyncHandler }
