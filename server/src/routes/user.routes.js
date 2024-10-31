8832c97dadd38712d693d959ef1013de06299ce7
Niraaj10 <nitcode10@gmail.com>
2024-10-31 16:57:02 +0530

    User logout route created

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { loginUser, registerUser } from '../controllers/user.controller.js'
+import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js'
+import { verfiyJWT } from '../middlewares/auth.mw.js'
 
 const router = Router()
 
 router.route('/sign-up').post(registerUser)
 router.route('/sign-in').post(loginUser)
+router.route('/logout').post(verfiyJWT, logoutUser)
+
+
 
 
 
