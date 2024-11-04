357d5fd9769b8201b0911bed872869786dcf23bb
Niraaj10 <nitcode10@gmail.com>
2024-11-04 20:36:57 +0530

    Delete Event route Created

--- server/src/controllers/event.controller.js
+++ server/src/controllers/event.controller.js
@@ -3,14 +3,14 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
-const createEvent = asyncHandler( async (req, res) => {
+const createEvent = asyncHandler(async (req, res) => {
     // res.status(201).json({
     //     message: "OKKK",
     // })
-    
+
     // console.log(req.body)
     const { title, description, location, date, categories, ticketPrice, totalSeats } = req.body
-    
+
     //validation checks - not empty
     if (
         [title, description, location, date, categories, ticketPrice, totalSeats].some((field) => field?.trim() === "")
@@ -21,22 +21,48 @@ const createEvent = asyncHandler( async (req, res) => {
     const event = await Event.create({
         title,
         description,
-        location, 
+        location,
         date,
         organizer: req.user._id,
-        categories, 
-        ticketPrice, 
+        categories,
+        ticketPrice,
         totalSeats,
         availableSeats: totalSeats
     })
 
     if (!event) {
-        throw new ApiError(500, "Something went wrong while registering the user")
+        throw new ApiError(500, "Something went wrong while creating the event")
     }
 
     return res.status(201).json(
-        new ApiResponse(200, event, "User registered successfully")
+        new ApiResponse(200, event, "Event created successfully")
     )
+
+})
+
+
+
+const getEventByTitle = asyncHandler(async (req, res) => {
+    //
+})
+
+
+const deleteEvent = asyncHandler(async (req, res) => {
+    const event = await Event.findById(req.params.eventId);
+    if (!event) throw new ApiError(404, "Event not found")
+
+    // Check if the event date has passed
+    const currentDate = new Date();
+    if (event.date < currentDate) {
+        await Event.findByIdAndDelete(req.params.eventId);
+        return res.status(200).json(
+            new ApiResponse(200, event, "Event date has passed and it was automatically deleted")
+        );
+    }
+
+    // If the event is still upcoming, perform the regular delete
+    const deletedEvent = await Event.findByIdAndDelete(req.params.eventId);
+    res.status(200).json(new ApiResponse(200, deletedEvent, "Event deleted successfully"));
 
 })
 
@@ -44,7 +70,8 @@ const createEvent = asyncHandler( async (req, res) => {
 
 
 
+
 export {
     createEvent,
-
+    deleteEvent
 }
--- server/src/routes/event.route.js
+++ server/src/routes/event.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verfiyJWT } from '../middlewares/auth.mw.js'
-import { createEvent } from "../controllers/event.controller.js";
+import { createEvent, deleteEvent } from "../controllers/event.controller.js";
 
 const router = Router()
 
 router.route('/create').post(verfiyJWT, createEvent)
+
+router.route('/delete').delete(verfiyJWT, deleteEvent);
 
 
 export default router
