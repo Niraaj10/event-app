0eb596ae9df006aa5b9834b9d20f33873f44e84c
Niraaj10 <nitcode10@gmail.com>
2024-12-02 17:41:36 +0530

    Navbar UI Created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,10 +5,13 @@ const Navbar = () => {
     <>
       <div className='navbar m-5 border rounded-2xl bg-nav text-white'>
 
-        <div className='flex justify-between items-center p-4 px-20 '>
-            <div className="div">Event</div>
+        <div className='flex justify-between items-center p-4 px-12'>
+            <div className="pr-14 font-bold text-lg">Events</div>
             <div className="logo-font font-extrabold text-5xl">Eventura</div>
-            <div className="div">User</div>
+            <div className="flex gap-2 justify-center items-center">
+              <button className='border-2 p-2 px-4 rounded-lg'>Login</button>
+              <button className='bg-white text-[#892eff] font-bold p-2 px-4 rounded-lg'>Signup</button>
+            </div>
         </div>
 
       </div>
