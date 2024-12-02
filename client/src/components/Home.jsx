cbdf699c994f07d44f3dd487f615d9901ccc160c
Niraaj10 <nitcode10@gmail.com>
2024-12-02 17:30:27 +0530

    Some ui updates on home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,13 +4,21 @@ import bg from '../assets/Images/pablo-heimplatz-ZODcBkEohk8-unsplash (1).jpg'
 const Home = () => {
   return (
     <>
-      <div>
+      <div className='mx-10'>
         {/* Home */}
 
-        <div className='flex justify-center '>
-          <img src={bg} alt="" className='w-[90vw] h-[70vh] rounded-2xl object-cover' />
+        <div className='flex justify-center relative z-0'>
+          <img src={bg} alt="" className='w-[90vw] h-[70vh] rounded-2xl object-cover z-0 static' />
         </div>
 
+        <div className='flex justify-center items-center'>
+
+          <div className='p-5 bg-white mt-[-125px] w-[70vw] relative z-10 border rounded-xl flex gap-4 justify-center'>
+            <input type="text" placeholder='Enter your location' className='p-2 border w-[40vw] rounded-lg'/>
+            <button className='bg-nav p-2 px-7 text-white rounded-lg'>Search Event</button>
+          </div>
+
+        </div>
       </div>
     </>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     <>
       <div className='navbar m-5 border rounded-2xl bg-nav text-white'>
 
-        <div className='flex justify-between items-center p-6 px-20 '>
+        <div className='flex justify-between items-center p-4 px-20 '>
             <div className="div">Event</div>
-            <div className="logo-font font-extrabold text-4xl ">Eventura</div>
+            <div className="logo-font font-extrabold text-5xl">Eventura</div>
             <div className="div">User</div>
         </div>
 
