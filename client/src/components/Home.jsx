7ca85a31dc19807add1ce67f4ff1a0a765b86687
Niraaj10 <nitcode10@gmail.com>
2024-12-03 18:29:37 +0530

    Some Updates in Home page UI

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
         {/* Home */}
 
         <div className='flex justify-center relative z-0'>
-          <img src={bg} alt="" className='w-[90vw] h-[70vh] rounded-2xl object-cover z-0 static' />
+          <img src={bg} alt="" className='w-[90vw] h-[80vh] rounded-2xl object-cover z-0 static' />
         </div>
 
         <div className='flex justify-center items-center'>
 
-          <div className='p-5 bg-white mt-[-125px] w-[70vw] relative z-10 border rounded-xl flex gap-4 justify-center'>
+          <div className='p-9 bg-white mt-[-155px] w-[70vw] relative z-10 border rounded-xl flex gap-4 justify-center'>
             <input type="text" placeholder='Enter your location' className='p-2 border w-[40vw] rounded-lg'/>
-            <button className='bg-nav p-2 px-7 text-white rounded-lg'>Search Event</button>
+            <button className='bg-nav p-3 px-7 text-white rounded-lg font-bold hover:scale-105 transition-transform duration-300'>Search Events In Your City </button>
           </div>
 
         </div>
